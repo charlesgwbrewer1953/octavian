35878403520492f46990bca2d98846d61b1a5d47
charles.brewer@namax.org <charles.brewer@namax.org>
2021-04-28 17:17:07 +0100

    Minor changes

--- ui.R
+++ ui.R
@@ -1,11 +1,18 @@
 #
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
 #
-# Find out more about building applications with Shiny here:
+# octavian
+# Git
+# /OneDrive/metis2/metis_select_retrieve
+# - resegment_observe branch
+
+# SERVER
 #
-#    http://shiny.rstudio.com/
 #
+# IMPORTANT NOTE - If plyr is loaded after dplyr (in tidyverse()), then group_by statement will fail
+# with group factor not carried across
+#
+#
+
 
 library(shiny)
 
